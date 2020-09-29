2f787a89f642cc7f7545f8a28d0c51dcb4559226
Vktrlg <71331086+Vktrlg@users.noreply.github.com>
2020-09-29 16:46:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 function num(x){
-	if(x%2===0) {
+	if (x % 2 === 0) {
 		console.log("Even"); 
 	}
 	else {
 		console.log("odd");
 	}
-	if(x==0){console.log("x = 0");}
-	else if(x<0) {console.log("x<0");}
-	else if(x>0){console.log("x>0");}
-	if(x % 1 === 0) {console.log("x is integer");}
-	else {console.log("x is float");}
+	if(x == 0){
+		console.log("x = 0");
+	}
+	else if(x < 0) {
+		console.log("x<0");
+	}
+	else if(x > 0){
+		console.log("x>0");
+	}
+	if(x % 1 === 0) {
+		console.log("x is integer");
+	}
+	else {
+		console.log("x is float");
+	}
 }
