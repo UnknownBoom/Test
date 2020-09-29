55af28d9dec7c87004c7a5cc8b5972d4b47a966d
Vktrlg <71331086+Vktrlg@users.noreply.github.com>
2020-09-29 16:37:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function multy(x,y){
-	var a  = Math.pow(x,y)
+	var a=0;
+	if(y===0) {
+		a=1;
+	}
+	else if(y<0){
+		a=1/x;
+		for(let i=1;i<y;i++){
+			a*=x;
+		}
+	}else{
+		a=x;
+		for(let i=1;i<y;i++){
+			a*=x;
+		}
+	}
 	console.log(a);
 }
 multy(2,3);
